ff75628bf5ec9faa7379dda9bdde24b98e20a070
Muhammad Asad <148639150+ASAD2204@users.noreply.github.com>
2026-09-09 16:11:51 +0500

    Time: 0 ms (100.00%) | Memory: 28.2 MB (44.27%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       unordered_map<int,int>frequency;
+       int freq=0;
+       int ans=0;
        for(int i=0;i<nums.size();i++){
-        frequency[nums[i]]++;
-       } 
-       int majority_count=INT_MIN;
-       int maj_element=0;
-       for(auto pair:frequency){
-        if(pair.second>majority_count){
-            majority_count=pair.second;
-            maj_element=pair.first;
+        if(freq==0){
+            ans=nums[i];
         }
-       
+        if(ans==nums[i]){
+            freq++;
+        }
+        else{
+            freq--;
+        }
        }
- return maj_element;
+    return ans;
     }
 };
