cb884a3d903e0843be123517b4885c1543fd0d3a
Muhammad Asad <148639150+ASAD2204@users.noreply.github.com>
2026-09-10 15:14:08 +0500

    Time: 3 ms (67.32%) | Memory: 15 MB (28.57%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,10 +3,10 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int>ans;
         unordered_map<int,int> dictionary;
-        int first,second=0;
+     
         for(int i=0;i<nums.size();i++){
-            first=nums[i];
-            second=target-first;
+          int  first=nums[i];
+           int second=target-first;
             if(dictionary.find(second)!=dictionary.end()){
                 ans.push_back(i);
                 ans.push_back(dictionary[second]);
